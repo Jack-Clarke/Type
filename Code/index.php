d66e54eefdca4fff78a9663741c4ee288dc0b38a
Phil Clarke <phil.clarke@edcoms.co.uk>
2013-12-28 10:09:10 +0000

    Counting characters

--- Code/index.php
+++ Code/index.php
@@ -44,19 +44,40 @@
 		}
 		html	{margin:0;padding:0;}
 		body	{margin:0;padding:0;valign:center;}
-		.type	{font-size:500%;font-family:"Courier New", Courier, monospace;width:70%;height:300px;display:block;border:1px solid #c1c1c1;margin:0 auto;background:#f1f1f1;}
+		.type	{font-size:225%;font-family:"Courier New", Courier, monospace;width:70%;height:300px;display:block;border:1px solid #c1c1c1;margin:0 auto;background:#f1f1f1;}
 		#page	{width:100%;padding:100px 0;}
+		#charNum	{width:70%;margin:10px auto;}
 	</style>
+    
+   
     
 </head>
 
 <body OnLoad="document.typeapp.texttype.focus();">
-	<div id="page">
-    <form name="typeapp" id="typeapp">
-    	<textarea wrap="off" draggable="false" id="texttype" name="texttype" spellcheck="false" class="type"></textarea>
+	<form name="typeapp" id="typeapp">
+    	<div id="page">
+    		<span><textarea wrap="off" draggable="false" id="texttype" name="texttype" spellcheck="false" class="type" onkeyup="countChar(this)"></textarea>
+        	<span id="texttypeSpan">&nbsp;</span></span>
+            <div id="charNum"></div>
+    	</div>
     </form>
-    </div>
+    
+    
+    
     <script src="/js/jquery-1.10.2.js"></script>
-
+    <script>
+      $('#texttype').keyup(function () {
+		  var max = 50;
+		  var len = $(this).val().length;
+		  if (len >= max) {
+			$('#charNum').text(' you have reached the limit');
+			// Do other stuff here
+		  } else {
+			var char = max - len;
+			$('#charNum').text(char + ' characters left');
+		  }
+		});
+    </script>
+	
 </body>
 </html>
